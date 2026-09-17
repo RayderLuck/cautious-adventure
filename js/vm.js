b89491213dff40e44571d5e91ad48f7adb777f90
Rayder <lucastrindaderayder@hotmail.com>
2026-09-16 21:20:46 -0300

    Add VM configuration and mount function

--- js/vm.js
+++ js/vm.js
@@ -1,1 +1,9 @@
+export function initVMConfig(){
+  return {
+    memory_size: 128 * 1024 * 1024,
+  };
+}
 
+export async function mountVaultIntoVM(emulator, decryptedFiles){
+  console.log("Montando vault na VM", decryptedFiles);
+}
